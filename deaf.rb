0dce6d35a2edcbefe1f4a9ceea485d8a61189ad2
Gorillq <kamil.kownacki3@gmail.com>
2025-01-17 19:24:11 +0100

    stupid but works

---
+++ deaf.rb
@@ -0,0 +1,61 @@
+class Solution
+  def initialize
+    @counter = 0
+  end
+  def reset
+    @counter = 0
+  end
+  def greeter
+    puts "Say sth to grandma!"
+  end
+  def huh
+    puts "HUH?! SPEAK UP KAMIRU!"
+  end
+  def notsince
+    year = 1920 + rand(35)
+    puts "NOT SINCE #{year}"
+  end
+  def scaner
+    input = gets.chomp() 
+    return input
+  end
+  def capital?(input)
+    if input == input.upcase
+      notsince
+      reset
+      return
+    end
+    huh
+    reset
+  end
+  def bye?(input)
+    keyword = "BYE"
+    if input == keyword
+      @counter += 1
+      if @counter >= 3
+        puts "GOOD BYE!"
+        Kernel.exit!(0)
+      end
+      huh
+      programLogic
+    end
+  end
+  def programLogic
+    loop do
+      greeter
+      tmp = scaner
+      if tmp == "" || tmp.nil?
+        programLogic
+      end
+      bye?(tmp)
+      capital?(tmp)
+    end
+  end
+end
+
+if __FILE__ == $0
+  klasa = Solution.new
+  klasa.programLogic
+end
+
+
--- string.rb
+++
@@ -1,5 +0,0 @@
-if __FILE__ == $0
-    puts "input string i enter"
-    scanner = gets.chomp.to_s()
-    puts scanner.reverse()
-end
